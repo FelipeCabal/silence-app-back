7273f3e01367ca3ec834c40b9ed0fb8d41a40b8e
Alejandro Moreno <alejandromogil@gmail.com>
2025-10-25 16:00:42 -0500

    feat: enhance PublicacionResponseDto and Publicacion schema with createdAt and updatedAt fields

--- src/publicaciones/dto/responses/publicacion-response.dto.ts
+++ src/publicaciones/dto/responses/publicacion-response.dto.ts
@@ -1,14 +1,60 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Publicacion } from 'src/publicaciones/entities/publicacion.schema';
 
 export class PublicacionResponseDto {
+  @ApiProperty({
+    description: 'ID de la publicación',
+    example: '64b7f8c2e1d3f2a5b6c7d8e9',
+  })
   id: string;
+
+  @ApiProperty({
+    description: 'Descripción de la publicación',
+    example: 'Esta es una publicación de ejemplo.',
+  })
   description: string;
+
+  @ApiProperty({
+    description: 'URL de la imagen asociada a la publicación',
+    example: 'https://example.com/image.jpg',
+    nullable: true,
+  })
   imagen: string | null;
+
+  @ApiProperty({
+    description: 'Comentarios de la publicación',
+    example: [],
+  })
   comentarios: any[];
+
+  @ApiProperty({
+    description: 'Cantidad de likes en la publicación',
+    example: 10,
+  })
   cantLikes?: number;
+
+  @ApiProperty({
+    description: 'Cantidad de comentarios en la publicación',
+    example: 5,
+  })
   cantComentarios?: number;
+
+  @ApiProperty({
+    description: 'Indica si la publicación es anónima',
+    example: false,
+  })
   esAnonimo: boolean;
+
+  @ApiProperty({
+    description: 'Fecha de creación de la publicación',
+    example: '2023-08-30T12:34:56Z',
+  })
   createdAt: Date;
+
+  @ApiProperty({
+    description: 'Fecha de actualización de la publicación',
+    example: '2023-08-30T12:34:56Z',
+  })
   updatedAt: Date;
 
   public static fromModel(model: Publicacion): PublicacionResponseDto {
@@ -20,7 +66,7 @@ export class PublicacionResponseDto {
     dto.cantLikes = model.cantLikes ?? 0;
     dto.cantComentarios = model.cantComentarios ?? 0;
     dto.esAnonimo = model.esAnonimo;
-    dto.createdAt = model.createdAt
+    dto.createdAt = model.createdAt;
     dto.updatedAt = model.updatedAt;
     return dto;
   }
