d6e71b7d112c9830d304859f8fe1d10719f81ba2
Felipe Cabal <felipe-cabal@hotmail.com>
2025-11-14 21:47:11 -0500

    fix(firebase): ajuste en module de firebase

--- src/firebase/firebase.module.ts
+++ src/firebase/firebase.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { FirebaseService } from './firebase.service';
 import { FirebaseController } from './firebase.controller';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from '../config/constants/jwt.constants';
+import { AuthGuard } from '../auth/guards/auth.guard';
 
 @Module({
+    imports: [
+        UsersModule,
+        JwtModule.register(jwtConstants),
+    ],
     controllers: [FirebaseController],
-    providers: [FirebaseService],
+    providers: [FirebaseService, AuthGuard],
     exports: [FirebaseService],
 })
 export class FirebaseModule { }
