f9af7ed4cb679f4aebd5203912b1c93ce78a6b4b
Felipe Cabal <felipe-cabal@hotmail.com>
2025-12-07 01:08:18 -0500

    feat: refactor Comentario model to use UsuarioComentario schema and update user property type

--- src/publicaciones/models/comentario.model.ts
+++ src/publicaciones/models/comentario.model.ts
@@ -1,13 +1,27 @@
-import { Prop } from '@nestjs/mongoose';
+import { Prop, Schema } from '@nestjs/mongoose';
 import { Types } from 'mongoose';
-import { User } from 'src/users/entities/user.model';
+
+@Schema({ _id: false })
+class UsuarioComentario {
+  @Prop({ type: String, required: true })
+  _id: string;
+
+  @Prop({ type: String, required: true })
+  nombre: string;
+
+  @Prop({ type: String, default: null })
+  imagen?: string;
+
+  @Prop({ type: String, required: true })
+  userId: string;
+}
 
 export class Comentario {
   @Prop({ type: Types.ObjectId, auto: true })
   _id: Types.ObjectId;
 
-  @Prop({ type: User, default: null })
-  usuario: User | null;
+  @Prop({ type: UsuarioComentario, required: true })
+  usuario: UsuarioComentario;
 
   @Prop({ required: true, trim: true })
   comentario: string;
